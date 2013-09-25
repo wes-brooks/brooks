bc32733d672afcbca43528f3cf8e94d520725fa3
Wesley Brooks <brooks.wesley@gmail.com>
2013-09-25 11:07:21 -0500

    Now load_https truly defaults to csv

--- code/load_https.r
+++ code/load_https.r
@@ -5,5 +5,5 @@ load_https <- function(url, sep=',', header=TRUE, row.names=NULL, ...) {
   # Import the data:
   read.table(text = getURL(url,
     followlocation=TRUE, cainfo=system.file("CurlSSL", "cacert.pem", package="RCurl")),
-    ...)
+    sep=sep, header=header, row.names=row.names, ...)
 }
